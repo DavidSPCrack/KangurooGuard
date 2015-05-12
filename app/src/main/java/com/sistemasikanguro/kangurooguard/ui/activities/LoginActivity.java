556213a1d54fbd34429ffe57e793a53f395eb1f0
Andres <andresalvarezperez@hotmail.com>
2015-05-12 22:13:26 +0200

    layout addrouter

--- app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/LoginActivity.java
+++ app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AbstractAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_options);
+        setContentView(R.layout.activity_addroute);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
