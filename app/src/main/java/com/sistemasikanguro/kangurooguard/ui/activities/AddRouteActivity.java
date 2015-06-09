7c0b15033edb1d0ae8f34e901cc11727472a1c6a
Andres <andresalvarezperez@hotmail.com>
2015-06-09 20:19:35 +0200

    layouts_nuevos

--- app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/AddRouteActivity.java
+++ app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/AddRouteActivity.java
@@ -31,34 +31,6 @@ public final class AddRouteActivity extends AbstractAppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_add_route, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.modify) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Modificar", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        if (id == R.id.add) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Añadir", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        if (id == R.id.delete) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Eliminar", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void changeRoute(View view) throws ErrorGeneral {
         Toast toast = Toast.makeText(getApplicationContext(), "Route Changed", Toast.LENGTH_SHORT);
         toast.show();
--- app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/DataStudentActivity.java
+++ app/src/main/java/com/sistemasikanguro/kangurooguard/ui/activities/DataStudentActivity.java
@@ -22,34 +22,6 @@ public final class DataStudentActivity extends AbstractAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_student);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_data_student, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.modify) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Modificar", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        if (id == R.id.add) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Añadir", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        if (id == R.id.delete) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Eliminar", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
